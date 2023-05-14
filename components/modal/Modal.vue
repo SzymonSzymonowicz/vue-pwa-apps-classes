c7c143e76f25ba60b53becd8aa3dccf56f6a9343
Szymon Szymonowicz <sz.szymonowicz5@gmail.com>
2023-05-14 11:23:47 +0200

    Restore modal to main photo

--- components/modal/Modal.vue
+++ components/modal/Modal.vue
@@ -1,65 +1,54 @@
 <template lang="">
-    <div class="modal">
-        <div class="modal-content" v-click-outside="closeModal">
-            <!-- can't add class to slot since vue 2, won't work -->
-            <slot>
-                Something
-            </slot>
-        </div>
+  <div class="modal">
+    <div class="modal-content" @click="closeModal">
+      <!-- click outside not working right now due to some bug -->
+      <!-- <div class="modal-content" v-click-outside="closeModal"> -->
+      <!-- can't add class to slot since vue 2, won't work -->
+      <slot> Something </slot>
     </div>
+  </div>
 </template>
 <script>
-    export default {
-        methods: {
-            closeModal: function () {
-                console.log("About to emit")
-                this.$emit('change-modal-state', true);
-
-                // if (this.$el.classList.contains('hidden')) {
-                //     return;
-                // }
-                // console.log("clicked outside");
-                // console.log(this.$el.classList);
-                // this.$el.classList.add('hidden');
-            },
-            // openModal: function () {
-            //     if (this.$el.classList.contains('hidden')) {
-            //         this.$el.classList.remove('hidden');
-            //         return;
-            //     }
-            // }
-        }
-    }
+export default {
+  methods: {
+    closeModal: function () {
+      //   console.log("About to emit");
+      this.$emit("change-modal-state", true);
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-    .modal {
-        // to be on top of everything
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100vw;
-        height: 100vh;
-        z-index: 1;
+.modal {
+  // to be on top of everything
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1;
 
-        display: flex;
-        justify-content: center;
-        align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-        background-color: rgba(0, 0, 0, 0.5);
-        // temporary, to delete
-        // display: none;
-    }
-    
-    .modal-content {
-        background: white;
-        max-height: 90vh;
-        width: auto;
-        display: flex;
-        overflow: hidden;
-        align-content: center;
-    }
+  background-color: rgba(0, 0, 0, 0.5);
+}
 
-    .hidden {
-        display: none;
-    }
+.modal-content {
+  background: white;
+  max-height: 90vh;
+  width: auto;
+  display: flex;
+  overflow: hidden;
+  align-content: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.hidden {
+  display: none;
+}
 </style>
--- components/photo_gallery/MainPhoto.vue
+++ components/photo_gallery/MainPhoto.vue
@@ -1,40 +1,44 @@
 <template>
-    <div>
-        <img class="big-photo" :src="photoSrc" @click="changeModalState" />
-        <Modal v-if="isModalOpened" @change-modal-state="changeModalState">
-            <img :src="photoSrc">
-        </Modal>
-    </div>
+  <div>
+    <img class="big-photo" :src="photoSrc" @click="changeModalState" />
+    <Modal v-if="isModalOpened" @change-modal-state="changeModalState">
+      <img :src="photoSrc" />
+    </Modal>
+  </div>
 </template>
 <script>
-import Modal from '../modal/Modal.vue';
+import Modal from "../modal/Modal.vue";
 
 export default {
-    name: "MainPhoto",
-    props: {
-        photoSrc: String,
+  name: "MainPhoto",
+  props: {
+    photoSrc: String,
+  },
+  components: { Modal },
+  data() {
+    return {
+      isModalOpened: false,
+    };
+  },
+  methods: {
+    changeModalState: function () {
+      //   console.log("About to change modal state to: " + !this.isModalOpened);
+      this.isModalOpened = !this.isModalOpened;
     },
-    components: { Modal },
-    data() {
-        return {
-            isModalOpened: false
-        }
-    },
-    methods: {
-        changeModalState: function () {
-            console.log("About to change modal state to: " + !this.isModalOpened)
-            this.isModalOpened = !this.isModalOpened;
-        },
-    }
-}
+  },
+};
 </script>
 <style lang="scss" scoped>
-    .big-photo {
-        width: 500px;
-        height: 500px;
-        max-height: 100%;
-        max-width: 100%;
-        justify-content: center;
-        padding: 20px;
-    }
+.big-photo {
+  width: 500px;
+  height: 500px;
+  max-height: 100%;
+  max-width: 100%;
+  justify-content: center;
+  padding: 20px;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
 </style>
